81bce15436d3995d399182b886a0a3263868fbda
jkroh1995 <85445649+jkroh1995@users.noreply.github.com>
2023-09-08 18:12:42 +0900

    Update Solution.java

--- 연습문제/최고의 집합/Solution.java
+++ 연습문제/최고의 집합/Solution.java
@@ -2,12 +2,14 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int n, int s) {
+        // 자연수 집합이기 때문에 n이 더 크면 집합을 만들 수 없음.
         if (s < n) {
             return new int[]{-1};
         }
 
         int[] answer = new int[n];
 
+        // 만일 s가 n으로 나누어 떨어지면 s를 n으로 나눠 균일한 값을 집합에 채움
         if (s % n == 0) {
             Arrays.fill(answer, s / n);
             return answer;
@@ -17,6 +19,7 @@ class Solution {
         s -= remainder;
         Arrays.fill(answer, s / n);
 
+        // remainder는 n보다 작을 수밖에 없기 때문에, 1씩 숫자를 늘려서 숫자 간 격차를 최소화 함
         for (int i = n - 1; i >= n - remainder; i--) {
             answer[i]++;
         }
